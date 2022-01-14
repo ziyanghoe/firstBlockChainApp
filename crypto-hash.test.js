bf125cdaa8452fcf2bd137e9b5da5665882a26a3
Hoe Zi Yang <hoeziyang97@gmail.com>
2022-01-14 23:31:57 +0800

    test

--- crypto-hash.test.js
+++ crypto-hash.test.js
@@ -9,3 +9,13 @@ describe('crytoHash()', () => {
         expect(crytoHash('one','two','three')).toEqual(crytoHash('two','one','three'));
     });
 });
+
+describe('crytoHash()', () => {
+    
+    it('generate SHA-356 hashed output', () => {
+        expect(crytoHash('foo')).toEqual('2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae');
+    });
+    it('produces the same ash with the same input arguments in any order', () => {
+        expect(crytoHash('one','two','three')).toEqual(crytoHash('two','one','three'));
+    });
+});
